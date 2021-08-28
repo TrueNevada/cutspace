0d808ef71c45ab3ccd1a9b358cae39e8c0b5fde9
TrueNevada <ricefieldsevilsamurai@gmail.com>
2021-08-28 08:37:21 +0300

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,20 @@ gulp.task('clean', function () {
   return del('build');
 });
 
+gulp.task('copy', function (){
+  return gulp.src([
+    'assets/fonts/**/*.{woff,woff2}',
+    'assets/images/**',
+    'assets/js/**'
+  ], {
+    base: 'assets'
+  })
+  .pipe(gulp.dest('build'));
+});
+
+
 gulp.task('sass', function () {
-  return gulp.src('sass/style.scss')
+  return gulp.src('assets/sass/style.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
@@ -29,20 +41,19 @@ gulp.task('html', function () {
     .pipe(browserSync.stream());
 });
 
+gulp.task('build', gulp.series(
+  'clean',
+  'copy',
+  'sass',
+  'html'
+));
+
 gulp.task('serve', function () {
   browserSync.init({
     server: 'build/'
   });
-  gulp.watch('sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('assets/sass/**/*.scss', gulp.series('sass'));
   gulp.watch('*.html', gulp.series('html'));
 });
 
-/*
-Browser Sync - это веб сервер, построенный на базе фреймворка Express
-Сервер в самом простом случае работает так: получает запрос и по умолчанию
-ищет index.html, если не находит - то возвращает ошибку 404, что и происходило.
-Ты запускал сервер, но в папке build не было index.html на тот момент, поэтому
-он отработал так, как и должен - отдал ошибку 404.
-*/
-
-gulp.task("start", gulp.series("clean","sass", "html", "serve"));
+gulp.task('start', gulp.series('clean', 'copy', 'sass', 'html', 'serve'));
